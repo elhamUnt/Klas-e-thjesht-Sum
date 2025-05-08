03906a69c88a8341d59960d5cf8eeadbc646ca86
elhamUnt <elham.hamiti@unt.edu.mk>
2025-05-08 13:30:15 +0000

    sum

--- main.cpp
+++ main.cpp
@@ -1,3 +1,29 @@
 #include <iostream>
+using namespace std;
 
-int main() { std::cout << "Hello World!\n"; }
+class Numra {
+public:
+  int numri1;
+  int numri2;
+  int S=0;
+
+  int shuma() {
+    S= numri1 + numri2;
+    return S;
+  }
+};
+
+int main() {
+  Numra n;
+
+  cout << "Shkruani numrin e pare: ";
+  cin >> n.numri1;
+
+  cout << "Shkruani numrin e dyte: ";
+  cin >> n.numri2;
+
+  cout << "Shuma e dy numrave eshte: " << n.shuma() << endl;
+  cout << "Shuma e dy numrave eshte: " << n.S << endl;
+
+  return 0;
+}
